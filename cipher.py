82160b6b59708e427dd0db4d445157203f93db9d
GiovanniCornejo <giocornejo424@gmail.com>
2024-04-04 01:00:11 -0400

    Added overlay window

--- cipher.py
+++ cipher.py
@@ -1,18 +1,14 @@
-def run_gui(background):
-    """
-    Runs the program with the provided background object using curses.wrapper().
-    
-    Parameters:
-        background (object): The background object, sometimes called stdscr.
-        
-    Returns:
-        None
-    """
-    pass
+import curses
+
+def main():
+    curses.wrapper(run_gui)
+    print("Thanks for using the XOR-Cipher App; See you next time!")
+
+# -----------------------CIPHER----------------------- #
 
 def cipher(message: bytes, key: bytes) -> bytes:
     """
-    Executes the Python cipher described earlier.
+    Executes the Python XOR-cipher.
     
     Parameters:
         message: The message to be ciphered.
@@ -36,11 +32,52 @@ def load_cipher_lib(library_path: str):
     """
     pass
 
+# -----------------------GUI----------------------- #
 
+def run_gui(background: curses.window):
+    """
+    Runs the program with the provided background object using curses.wrapper().
+    
+    Parameters:
+        background (object): The background object.
+        
+    Returns:
+        None
+    """
+    curses.curs_set(0) # No cursor
+    curses.noecho() # Type without it showing
+    curses.cbreak() # Cbreak to exit
+
+    key = "But there's one sound that no one knows... What does the Fox say?".encode('cp437')
+    text = "This is a haiku; it is not too long I think; but you may disagree".encode('cp437')
+
+    overlay_window = print_overlay()
+
+    # Run Application
+    button = "startup"
+    while button.lower() != 'q':
+        background.clear()
+
+        background.refresh()
+        overlay_window.refresh()
+        button = background.getkey()
+        
+def print_overlay():
+    overlay_window = curses.newwin(24, 80, 0, 0)
+    overlay_window.box()
+
+    # Welcome Message
+    _, w = overlay_window.getmaxyx()
+    intro_message = "Welcome to the XOR-Cipher App!"
+    x = w // 2 - len(intro_message) // 2
+    y = 1
+    overlay_window.addstr(y, x, intro_message)
+
+    return overlay_window
 
 
 
 
 
 if __name__ == "__main__":
-    pass
+    main()
