288a26e8f63a9d9b41294f8b3e4445531af5c859
GiovanniCornejo <giocornejo424@gmail.com>
2024-04-04 09:35:24 -0400

    Moved exit message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from gui import GUI
 
 def main(stdscr):
     GUI(stdscr).run()
-    print("Thanks for using the XOR-Cipher App; See you next time!")
 
 if __name__ == "__main__":
     curses.wrapper(main)
+    print("Thanks for using the XOR-Cipher App; See you next time!")
