91231e9abfbd289feb04ada9b0f872df06b78f35
GiovanniCornejo <giocornejo424@gmail.com>
2024-04-04 17:36:47 -0400

    Added prompt and input window, status

--- gui.py
+++ gui.py
@@ -2,13 +2,27 @@ import curses
 
 class GUI:
     # Container for application
-    OVERLAY_ROWS, OVERLAY_COLS = 25, 80
+    OVERLAY_ROWS, OVERLAY_COLS = 24, 80
     # Display menu below top of overlay 
     MENU_ROWS, MENU_COLS = 10, 40
-    MENU_Y, MENU_X = 2, MENU_COLS // 2
-    # Display text, key below menu
-    DISPLAY_ROWS, DISPLAY_COLS = 4, OVERLAY_COLS - 4
-    DISPLAY_Y, DISPLAY_X = MENU_ROWS + MENU_Y, 2
+    MENU_Y, MENU_X = 2, 20
+    # Display output of text, key below menu
+    OUTPUT_ROWS, OUTPUT_COLS = 4, 76
+    OUTPUT_Y, OUTPUT_X = MENU_ROWS + MENU_Y, 2
+    # Display prompt below text, key
+    PROMPT_ROWS, PROMPT_COLS = 6, 78
+    PROMPT_Y, PROMPT_X = OUTPUT_ROWS + OUTPUT_Y + 1, 1
+    # Display user input below prompt
+    INPUT_ROWS, INPUT_COLS = 3, 68
+    INPUT_Y, INPUT_X = PROMPT_Y + 2, 6
+    """
+    Start on 18th row, 2nd column and be 6 x 78 
+2) Display prompt, centered, on 2nd row 
+3) Display box, centered, under prompt, size 3x68 
+4) Allow entry of 65 characters on one line only 
+"""
+
+    # ----------------------------------- Init ----------------------------------- #
 
     def __init__(self, stdscr: curses.window):
         curses.curs_set(0) # No cursor
@@ -19,27 +33,41 @@ class GUI:
         self.key = "But there's one sound that no one knows... What does the Fox say?".encode('cp437')
         self.text = "This is a haiku; it is not too long I think; but you may disagree".encode('cp437')
 
+        self.button = "startup"
+
         # Initialize Menus
         self.background = stdscr
+        self.status = "Application started successfully."
+        self.background.addstr(self.OVERLAY_ROWS, 0, self.status)
+
         self.overlay = self.init_overlay()
         self.menu = self.init_menu()
-        self.display = self.init_display()
+        self.output = self.init_output()
+        self.prompt = self.init_prompt()
+        self.input = self.init_input()
+
+        # Initial Display
+        self.update_status()
+        self.update_text_and_key()
+
+        self.background.refresh()
+        self.overlay.refresh()
+        self.menu.refresh()
+        self.output.refresh()
 
     def init_overlay(self):
         """Initialize the overlay window."""
         window = curses.newwin(self.OVERLAY_ROWS, self.OVERLAY_COLS, 0, 0)
         window.box()
 
-        intro_message = "Welcome to the XOR-Cipher App!"
-        x = self.OVERLAY_COLS // 2 - len(intro_message) // 2 # Center text
-        y = 1
-        window.addstr(y, x, intro_message)
+        message = "Welcome to the XOR-Cipher App!"
+        x = self.OVERLAY_COLS // 2 - len(message) // 2 # Center text
+        window.addstr(1, x, message)
 
         return window
     
     def init_menu(self):
         """Initialize the menu window contained within the overlay window."""
-        _, w = self.overlay.getmaxyx()
         window = curses.newwin(self.MENU_ROWS, self.MENU_COLS, self.MENU_Y, self.MENU_X)
         window.box()
     
@@ -59,14 +87,99 @@ class GUI:
         
         return window
     
-    def init_display(self):
+    def init_output(self):
         """Initialize the display window contained within the overlay window."""
-        window = curses.newwin(self.DISPLAY_ROWS, self.DISPLAY_COLS, 
-                               self.DISPLAY_Y, self.DISPLAY_X)
+        window = curses.newwin(self.OUTPUT_ROWS, self.OUTPUT_COLS, 
+                               self.OUTPUT_Y, self.OUTPUT_X)
         window.box()
         return window
     
-    def update_display(self):
+    def init_prompt(self):
+        """Initialize the prompt window contained within the overlay window."""
+        window = curses.newwin(self.PROMPT_ROWS, self.PROMPT_COLS, self.PROMPT_Y, self.PROMPT_X)
+        window.box()
+        return window
+    
+    def init_input(self):
+        """Initialize the user input window contained within the prompt window"""
+        window = curses.newwin(self.INPUT_ROWS, self.INPUT_COLS, self.INPUT_Y, self.INPUT_X)
+        window.box()
+        return window
+    
+
+    # ------------------------------ Run Application ----------------------------- #
+    
+    def run(self):
+        """
+        Runs the program with the provided background object.
+        """
+        while self.button != 'Q':
+            # Cipher Commands
+            if self.button == 'startup':
+                pass
+            # Input Commands
+            else:
+                self.run_prompt()
+
+            self.button = self.background.getkey().upper()
+
+    def run_prompt(self):
+        """
+        Handle the current input command or benchmarking
+
+        Parameters:
+        message: The message to prompt to the user.        
+        """
+
+        # Get Current Prompt
+        if self.button == 'F':
+            message = "Enter file to load, then press [ENTER]"
+
+        x = self.PROMPT_COLS // 2 - len(message) // 2 # Center text
+        self.prompt.addstr(1, 1, " " * (self.PROMPT_COLS - 3))
+        self.prompt.addstr(1, x, message)
+        self.prompt.refresh()
+
+        # TODO: Implement each input command
+        # TODO: Implement special display for benchmarking
+
+        self.input.refresh()
+    
+    # ------------------------------ Input Commands ------------------------------ #
+
+    def read_from_file(self):
+        """Read text from a local file."""
+        pass
+
+    def read_from_input(self):
+        """Read text from user input prompt."""
+        pass
+
+    def change_cipher_key(self):
+        """Change key used for ciphers."""
+        pass
+
+    # ------------------------------ Cipher Commands ----------------------------- #
+
+    def apply_rust_cipher(self):
+        """Apply Rust cipher to text."""
+        pass
+
+    def apply_python_cipher(self):
+        """Apply Python cipher to text"""
+        pass
+
+    def verify_cipher_results(self):
+        """Verify cipher results."""
+        pass
+
+    def run_benchmarks(self):
+        """Run benchmarks on text for Rust and Python cipher."""
+        pass
+
+    # ------------------------------ Update Display ------------------------------ #
+
+    def update_text_and_key(self):
         """Update the display window with the current text and key."""
         ctrl_translation = str.maketrans(
             bytes(range(0, 32)).decode("cp437"),
@@ -82,27 +195,13 @@ class GUI:
         if len(str_key) > 65:
             str_key = str_key[0:65]
 
-        # Clear previous text and key
-        self.display.move(1, 2)
-        self.display.addstr(" " * (self.display.getmaxyx()[1] - 3))
-        self.display.move(2, 2)
-        self.display.addstr(" " * (self.display.getmaxyx()[1] - 3))
+        self.output.addstr(1, 2," " * (self.OUTPUT_COLS - 3))
+        self.output.addstr(1, 2, "TEXT [" + str_text + "]")
 
-        self.display.addstr(1, 2, "TEXT [" + str_text + "]")
-        self.display.addstr(2, 2, "KEY  [" + str_key + "]")
-        self.display.refresh()
-
-    def run(self):
-        """
-        Runs the program with the provided background object.
-        """
-
-        button = "startup"
-        self.background.refresh()
-        self.overlay.refresh()
-        self.menu.refresh()
-        while button.lower() != 'q':
-
-
-            self.update_display()
-            button = self.background.getkey()
+        self.output.addstr(2, 2," " * (self.OUTPUT_COLS - 3))
+        self.output.addstr(2, 2, "KEY  [" + str_key + "]")
+            
+    def update_status(self):
+        """Update the status shown."""
+        self.background.addstr(self.OVERLAY_ROWS, 0, " " * self.OUTPUT_COLS)
+        self.background.addstr(self.OVERLAY_ROWS, 0, "Status: " + self.status)
